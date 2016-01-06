734a9a6241ed7fe0bdb54d1831c46e957ade20d8
Rob Parker <rparker@altiscale.com>
2016-01-06 15:10:21 -0600

    SI-514 add name to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "ark"
 maintainer       "Bryan W. Berry"
 maintainer_email "bryan.berry@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures ark"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.7"
+version          "0.0.8"
 
 
 %w{ java }.each do |cb|
